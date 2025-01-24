0e2e2a0cfdb71b47f1d2900b741c86ab44a1a316
k1ego <ladonin05@mail.ru>
2025-01-25 01:40:01 +0300

    Реализация алгоритмма BFS на бинарном дереве поиска

---
+++ BreadthFirstTree/levelOrderTraversal.js
@@ -0,0 +1,44 @@
+class TreeNode {
+	constructor(value) {
+			this.value = value;
+			this.left = null;
+			this.right = null;
+	}
+}
+
+// BFS
+function levelOrderTraversal(root) {
+	const result = [];
+	const queue = [root];
+
+	if (!root) {
+			return result;
+	}
+
+	while (queue.length > 0) {
+
+			const node = queue.shift();
+			result.push(node.value);
+
+			if (node.left) {
+					queue.push(node.left);
+			}
+			
+			if (node.right) {
+					queue.push(node.right);
+			}
+	}
+
+	return result;
+}
+
+// Пример использования:
+const root = new TreeNode(5);
+root.left = new TreeNode(3);
+root.right = new TreeNode(6);
+root.left.left = new TreeNode(2);
+root.left.right = new TreeNode(4);
+root.right.right = new TreeNode(7);
+root.left.left.left = new TreeNode(1);
+
+console.log(levelOrderTraversal(root)); // Вывод: [5, 3, 6, 2, 4, 7, 1]
